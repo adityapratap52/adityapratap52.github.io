e8d4224a985262b1dd6bfc9711826790cb1fe1ee
adityapratap52 <adityapratap1490@gmail.com>
2025-04-04 23:52:51 +0530

    resolve some bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,11 +166,9 @@ function showProjects(projects) {
           <div class="project-tech-stack">
           <div class="btns">
 <a href="${project.links.view}" class="btn project-deployed-link" target="_blank"><i class="fas fa-eye"></i> View</a>
-  <a  href="${project.links.code}" class="btn project-github-link" target="_blank">Code <i class="fas fa-code"></i></a>
-          
-
-         
-
+ <a href="${project.links.code}" class="btn project-github-link" target="_blank" ${project.visibleCode === false ? 'style="pointer-events: none; opacity: 0.5;"' : ''}>
+   Code <i class="fas fa-code"></i>
+  </a>
           </div>
           </div>
         
